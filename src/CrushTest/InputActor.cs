f3a6d3d8191d2b39eec02c69fb18d394fa80eabc
Aaron Stannard <aaron@petabridge.com>
2023-02-20 16:20:36 -0600

    initial design

---
+++ src/CrushTest/InputActor.cs
@@ -0,0 +1,24 @@
+﻿using Akka.Event;
+using Akka.Persistence;
+
+namespace CrushTest;
+
+public class InputActor : ReceivePersistentActor
+{
+    private readonly ILoggingAdapter _log = Context.GetLogger();
+    
+    public InputActor(string persistenceId)
+    {
+        PersistenceId = persistenceId;
+
+        Command<int>(str =>
+        {
+            Persist(str, s =>
+            {
+                _log.Info("Received {0}", s);
+            });
+        });
+    }
+
+    public override string PersistenceId { get; }
+}
---
+++ src/CrushTest/QueryActor.cs
@@ -0,0 +1,37 @@
+﻿using Akka;
+using Akka.Actor;
+using Akka.Persistence.Query;
+using Akka.Persistence.Query.Sql;
+using Akka.Streams;
+using Akka.Streams.Dsl;
+
+namespace CrushTest;
+
+public class QueryActor : ReceiveActor
+{
+    private readonly string _targetPersistentId;
+    private int _total = 0;
+
+    public QueryActor(string targetPersistentId)
+    {
+        _targetPersistentId = targetPersistentId;
+        
+        Receive<EventEnvelope>(e =>
+        {
+            _total++;
+            if (_total == 10)
+            {
+                Context.System.Log.Info("Completed recovery for entity {0}", _targetPersistentId);
+                Context.Stop(Self);
+            }
+        });
+    }
+
+    protected override void PreStart()
+    {
+        var query = Context.System.ReadJournalFor<SqlReadJournal>(SqlReadJournal.Identifier);
+        query.EventsByPersistenceId(_targetPersistentId, 0, long.MaxValue)
+            .To(Sink.ActorRef<EventEnvelope>(Self, Done.Instance))
+            .Run(Context.Materializer());
+    }
+}
