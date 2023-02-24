171abd202baba1babc1a7ef0ff6a23a12181cbf8
Aaron Stannard <aaron@petabridge.com>
2023-02-24 08:15:59 -0600

    added code to help implement 100,000 actors with 100 events (#13)
    10m total events

--- src/CrushTest/InputActor.cs
+++ src/CrushTest/InputActor.cs
@@ -8,9 +8,48 @@ public class InputActor : ReceivePersistentActor
 {
     private readonly ILoggingAdapter _log = Context.GetLogger();
     
+    private int _count = 0;
+    
     public InputActor(string persistenceId)
     {
         PersistenceId = persistenceId;
+        
+        Recover<int>(i =>
+        {
+            _count += 1;
+        });
+
+        Recover<RecoveryCompleted>(_ =>
+        {
+            if (_count == 100)
+            {
+                Context.Stop(Self);
+                _log.Info("Recovered all events for {0} - shutting down", PersistenceId);
+            }
+            else if (_count > 100)
+            {
+                _log.Info("Discovered excess events [{1}] for {0} - shutting down", PersistenceId, _count);
+                DeleteMessages(LastSequenceNr);
+            }
+            else
+            {
+                _log.Info("Only recovered {0} events out of 100 for {1} - backfilling", _count, PersistenceId);
+                var remainer = Enumerable.Range(0, 100 - _count).Chunk(10);
+                foreach(var c in remainer)
+                    Self.Tell(c);
+            }
+                
+        });
+        
+        Command<IEnumerable<int>>(i =>
+        {
+            var items = i.ToArray();
+            _log.Info("Writing {0} items", items.Length);
+            
+            PersistAll(items, s =>
+            {
+            });
+        });
 
         Command<string>(str =>
         {
@@ -23,11 +62,11 @@ public class InputActor : ReceivePersistentActor
         });
     }
 
-    public override Recovery Recovery => Recovery.None;
+    //public override Recovery Recovery => Recovery.None;
 
     protected override void PreStart()
     {
-        Self.Tell("write");
+       
     }
 
     public override string PersistenceId { get; }
